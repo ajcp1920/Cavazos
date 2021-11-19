23cd99277b39b70c2ea44195d0f6ecbbabbb2e1f
ajcp1920 <ajcptec19@gmail.com>
2021-11-19 00:21:24 -0600

    Script Formulario

--- Parcial 3/DataBase/web18100158.sql
+++ Parcial 3/DataBase/web18100158.sql
@@ -1,16 +1,29 @@
 create database FormularioEnLinea;
 use FormularioEnLinea;
 
-create table Datos(
-nomcom varchar(50),
-numerotel varchar(10),
-direccion varchar(50),
-ciudad varchar(30),
-estado varchar(30),
-codpos int,
-email varchar(30),
-comentariosextras varchar(100)
+create table Usuario(
+id int not null,
+pass varchar(10) not null,
+primary key(id)
 );
+
+create table Datos(
+nomcom varchar(50) not null,
+numerotel varchar(10) not null,
+direccion varchar(50) not null,
+ciudad varchar(30) not null,
+estado varchar(30) not null,
+codpos int not null,
+email varchar(30) not null,
+comentariosextras varchar(100) not null,
+foreign key (id) references Usuario (id)
+);
+
+insert into Usuario values (1, '5fg5jko9nb');
+insert into Usuario values (2, 'dsgkjdkgdf');
+insert into Usuario values (3, 'fdgdfgfdgg');
+insert into Usuario values (4, 'bvfgdzvghd');
+insert into Usuario values (5, 'osdpsndffg');
 
 insert into Datos values ('Arturo de Jesus Cavazos Pastrana','8971074895','Independencia #403','Cd. Mier','Tamaulipas',88390,'ajcp1920@gmail.com','Casa naranja arbol grande');
 insert into Datos values ('Cristina Cavazos Pastrana','8971003456','Mezquital','Cd. Miguel Aleman','Tamaulipas',88785,'crispycrim@gmail.com','Casa con porton electrico');
