ea3bd0b05b8cac99d8eb210e99c67dc3d8a17826
ajcp1920 <ajcptec19@gmail.com>
2021-12-02 23:23:01 -0600

    loginsql

--- Parcial 3/LoginSQL/index.php
+++ Parcial 3/LoginSQL/index.php
@@ -1,13 +1,42 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
+<?php
+session_start();
+if ( !isset($_SESSION['idUsuario']) || empty($_SESSION['idUsuario']) ) {
+    header("Location: ./login.html");
+}
+?>
 <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Formulario</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet"  href="estilo.css">
-<body>
-     <div class="container">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Formulario</title>
+
+    <link rel='stylesheet' href='./css/ModalHeaderColor.css'/>
+
+    <link rel="stylesheet" href="./resources/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./css/index.css">
+    <script src="./js/index.js"></script>
+</head>
+ <!-- Modal de Confirmación -->
+ <div class="modal fade" id="modalSalida" role="dialog">
+        <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header modal-header-primary">
+                <button type="button" class="close" data-dismiss="modal">X</button>
+                <h2 class="modal-title text-center">Confirmación</h2>
+            </div>
+            <div class="modal-body text-center">
+                <h3>Desea salir del Sistema?</h3>
+            </div>
+            <div class="modal-footer">
+               <a href="./php/logout.php" class="btn btn-default">Si</a>
+               <a href="#" data-dismiss="modal" class="btn btn-default">No</a>
+            </div>
+        </div>
+        </div>
+        </div>
+        <div class="container">
         <form class="form">
         
             <p class="uno"></p><h1>Datos para envio de Paquetes de Mercado Libre</h1></p>
